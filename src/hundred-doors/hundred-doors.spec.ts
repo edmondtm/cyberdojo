9ff19bfc9ef2c6a54babeda80e2811887400b1a3
Edmond Teh <edmond@letsrotate.com>
2024-12-01 13:18:41 +0800

    hundred doors

---
+++ src/hundred-doors/hundred-doors.spec.ts
@@ -0,0 +1,31 @@
+import { HundredDoors } from './hundred-doors';
+
+describe('HundredDoors', () => {
+  it('should be defined', () => {
+    expect(new HundredDoors()).toBeDefined();
+  });
+
+  it('100 doors in a row are all initially closed', () => {
+    const hd = new HundredDoors();
+    expect(hd.getDoors().length).toBe(100);
+    expect(hd.getDoors().every((door) => door.isOpen === false)).toBe(true);
+  });
+
+  it('visit doors should toggle isOpen to n % counter', () => {
+    
+    const hd = new HundredDoors();
+    let beforeVisit, afterVisit;
+
+    for (let counter = 1; counter <= 100; counter++) {
+      beforeVisit = hd.getDoors().map((door) => ({ ...door }));
+      afterVisit = hd.visitDoors();
+      afterVisit.forEach((door, index) => {
+        if ((index + 1) % counter === 0) {
+          expect(door.isOpen).toBe(!beforeVisit[index].isOpen);
+        } else {
+          expect(door.isOpen).toBe(beforeVisit[index].isOpen);
+        }
+      });
+    }
+  });
+});
---
+++ src/hundred-doors/hundred-doors.ts
@@ -0,0 +1,34 @@
+interface iHundredDoors {
+  getDoors(): Door[];
+  visitDoors(): Door[];
+}
+
+interface Door {
+  isOpen: boolean;
+}
+
+export class HundredDoors implements iHundredDoors {
+  private readonly doors: Door[];
+  private counter: number;
+
+  constructor() {
+    this.doors = Array.from({ length: 100 }, () => ({
+      isOpen: false
+    }));
+    this.counter = 0;
+  }
+
+  visitDoors(): Door[] {
+    this.counter++;
+    this.doors.forEach((door, index) => {
+      if ((index + 1) % this.counter === 0) {
+        door.isOpen = !door.isOpen;
+      }
+    });
+    return [...this.doors];
+  }
+
+  getDoors(): Door[] {
+    return [...this.doors];
+  }
+}
